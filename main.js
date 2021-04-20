94cf3100285584e9d7c2d1f7cddf5d2320f2d8db
sanidhya22-11 <77880747+sanidhya22-11@users.noreply.github.com>
2021-04-20 20:50:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function addUser(){
+    player1_name=document.getElementById("player1_name_input").value;
+    player2_name=document.getElementById("player2_name_input").value;
+    localStorage.sentItems("player1_name",player1_name);
+    localStorage.sentItems("player2_name",player2_name);
+    window.location.replace("game_page.html")};    
